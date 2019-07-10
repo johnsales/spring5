e6eeeef17d6e9a0e9023a9241bdef9a608f3c6da
John Santiago <jmsantiago@info.ufrn.br>
2019-07-10 09:34:14 -0300

    adding controller authors and books

---
+++ spring5webapp/src/main/java/john/spring/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,23 @@
+package john.spring.spring5webapp.controllers;
+
+import john.spring.spring5webapp.repositories.AuthorRepository;
+import john.spring.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+}
